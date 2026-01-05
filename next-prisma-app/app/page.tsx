afd56888b771c8e4c9e4972ad9a8666602eed5ec
csakyo <c.sakyou@gmail.com>
2026-01-05 19:59:48 +0900

    add: page.tsx

---
+++ next-prisma-app/app/page.tsx
@@ -0,0 +1,12 @@
+import { prisma } from './lib/prisma';
+
+export default async function Home() {
+  const users = await prisma.user.findMany();
+
+  return (
+    <main>
+      <h1>Users</h1>
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+    </main>
+  );
+}

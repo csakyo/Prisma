cedfc7c32df9a429e2179dab90659a8d0fceaf09
csakyo <c.sakyou@gmail.com>
2026-01-08 21:07:09 +0900

    add: 一覧ページ

---
+++ next-prisma-app/app/todo/page.tsx
@@ -0,0 +1,19 @@
+import { getTodos } from './actions';
+
+export default async function TodoPage() {
+  const userId = 1; // 仮ログイン
+  const todos = await getTodos(userId);
+
+  return (
+    <div>
+      <h1>Todo</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.completed ? '✅' : '⬜️'} {todo.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}

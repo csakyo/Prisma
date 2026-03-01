32ea2cf875970b4e55d7cd014ff036026290ef43
csakyo <c.sakyou@gmail.com>
2026-03-01 20:22:42 +0900

    fix: スタイル変更

--- next-prisma-app/app/todo/page.tsx
+++ next-prisma-app/app/todo/page.tsx
@@ -59,7 +59,7 @@ export default async function Page() {
               <input type='hidden' name='id' value={todo.id} />
               <button
                 type='submit'
-                className='ml-2 px-2 bg-red-500 text-black rounded text-sm'
+                className='ml-2 px-2 bg-white-500 text-black rounded text-sm border-solid border-2 border-gray-300 hover:bg-gray-100'
               >
                 削除
               </button>

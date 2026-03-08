874ac9895c4463cb6e3e77781b86c498635576fb
csakyo <c.sakyou@gmail.com>
2026-03-08 12:28:07 +0900

    add: seedファイル追加

---
+++ next-prisma-app/prisma/seed.ts
@@ -0,0 +1,29 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      email: 'test@test.com',
+    },
+  });
+
+  await prisma.todo.create({
+    data: {
+      title: '最初のTodo',
+      userId: user.id,
+    },
+  });
+
+  console.log('Seed data created');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });

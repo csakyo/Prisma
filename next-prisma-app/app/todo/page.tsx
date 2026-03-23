0a1e539f0848d9d4197c873e3302815cbf22a308
csakyo <c.sakyou@gmail.com>
2026-03-23 21:05:41 +0900

    fix: スタイル追加

--- next-prisma-app/app/todo/page.tsx
+++ next-prisma-app/app/todo/page.tsx
@@ -12,9 +12,17 @@ export default async function Page() {
     <div>
       <h1 className='text-2xl font-black'>Todo</h1>
       {/* 🔥 ここに追加フォーム */}
-      <form action={addTodo}>
-        <input name='title' />
-        <button type='submit'>追加</button>
+      <form action={addTodo} className='rounded p-4'>
+        <input
+          name='title'
+          className='border border-gray-300 rounded px-2 py-1'
+        />
+        <button
+          type='submit'
+          className='border border-blue-500 bg-blue-500 text-white rounded px-4 py-1 ml-2 hover:bg-blue-600'
+        >
+          追加
+        </button>
       </form>
       <TodoList todos={todos} />
     </div>
